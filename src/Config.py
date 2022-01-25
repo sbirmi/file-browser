8fddfb7a83ad7767d009a5565595040a7901b9ad
Sharad Birmiwal <sb-github@sharadb.net>
2022-01-24 20:20:27 -0800

    Change data + sqlite3 directory to inside data/ subdirectory

--- src/Config.py
+++ src/Config.py
@@ -1,8 +1,9 @@
 import os
 
-metadata_file = "00_metadata.sqlite3"
-upload_dir = "./uploads"
-thumbnail_dir = "./thumbnails"
+root_dir = "./data"
+metadata_file = os.path.join(root_dir, "00_metadata.sqlite3")
+upload_dir = os.path.join(root_dir, "uploads")
+thumbnail_dir = os.path.join(root_dir, "thumbnails")
 
 def upload_path(fname):
     return os.path.join(upload_dir, fname)
@@ -11,10 +12,11 @@ def thumbnail_path(fname):
     return os.path.join(thumbnail_dir, fname)
 
 try:
-    os.mkdir(upload_dir)
+    os.system("mkdir -p " + upload_dir)
 except:
     pass
+
 try:
-    os.mkdir(thumbnail_dir)
+    os.system("mkdir -p " + thumbnail_dir)
 except:
     pass
